408eb4710d990e18499356ba7aa9ef8204d138f2
Juan <juan.sonr@gmail.com>
2018-11-17 00:15:38 +0100

    parametros entrada calculo

---
+++ backend/src/main/java/com/hack2progress/model/dto/CalculoDTO.java
@@ -0,0 +1,8 @@
+package com.hack2progress.model.dto;
+
+public class CalculoDTO {
+	private int superficie;
+	private String orientacion;
+	private String aislamiento;
+	private PosicionDTO posicion;
+}
