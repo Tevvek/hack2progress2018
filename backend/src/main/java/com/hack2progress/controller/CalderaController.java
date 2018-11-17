67776f7f39f43c5586b86dbe9fc34dd5be954add
Juan <juan.sonr@gmail.com>
2018-11-17 04:12:16 +0100

    contexto de prueba

--- backend/src/main/java/com/hack2progress/controller/CalderaController.java
+++ backend/src/main/java/com/hack2progress/controller/CalderaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,12 @@ public class CalderaController {
 	
 	@Autowired
 	private CalderaService calderaService;
+	
+	@GetMapping("/pruebas")
+	public int pruebas(@RequestBody CalculoDTO calculo) {
+		System.out.println("entramossssssssss");
+		return 1;
+    }
 	
 	@PostMapping(path="/calcular", consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
     public List<CalderaResponse> calcular(@RequestBody CalculoDTO calculo) {
