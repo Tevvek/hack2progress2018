132b70db1fa634a7d71be5fe08d315ae18eac20d
Unknown <daniil.teterevkov@gmail.com>
2018-11-17 02:52:53 +0100

    Inicio prueba css grid

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,7 +5,22 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { ContentComponent } from './content/content.component';
 import { WizardComponent } from './content/wizard/wizard.component';
-import { PagesComponent } from './content/pages/pages.component';
+
+import { RouterModule, Routes } from '@angular/router';
+import { CalderasComponent } from './content/calderas/calderas.component';
+import { PlacasComponent } from './content/placas/placas.component';
+import { SectionsComponent } from './content/sections/sections.component';
+
+const appRoutes: Routes = [
+  { path: 'calderas', component: CalderasComponent },
+  { path: 'placas', component: PlacasComponent },
+  { path: '',
+    redirectTo: '/calderas',
+    pathMatch: 'full'
+  }
+  // ,
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
@@ -13,9 +28,15 @@ import { PagesComponent } from './content/pages/pages.component';
     MenuComponent,
     ContentComponent,
     WizardComponent,
-    PagesComponent
+    CalderasComponent,
+    PlacasComponent,
+    SectionsComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
---
+++ frontend/src/app/content/calderas/calderas.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CalderasComponent } from './calderas.component';
+
+describe('CalderasComponent', () => {
+  let component: CalderasComponent;
+  let fixture: ComponentFixture<CalderasComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CalderasComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CalderasComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
